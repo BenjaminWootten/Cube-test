7b351e2274f22335112f371d1c2fe23de46d540f
BenjaminWootten <bwootten@udel.edu>
2023-11-02 21:12:00 -0400

    Cube has faces between the lines

--- cube.py
+++ cube.py
@@ -7,11 +7,13 @@ from dataclasses import dataclass
 @dataclass
 class World:
     angle: float
+    scale: float
     vertices: list[DesignerObject]
     lines: list[DesignerObject]
+    faces: list[DesignerObject]
 
-scale = 100
 circle_pos = [get_width()/2, get_height()/2]
+
 
 
 projection_matrix = np.matrix([
@@ -37,48 +39,53 @@ projected_points = [
 ]
 
 
-def connect_points(i, j, points) -> DesignerObject:
+def connect_points(i: int, j: int, points) -> DesignerObject:
+    # Returns a line connecting points at indexes i and j in list points
     return line("black", points[i][0], points[i][1], points[j][0], points[j][1])
+
+def create_face(color: str, i: int, j: int, k: int, l: int, points) -> DesignerObject:
+    # Returns a shape of chosen color connecting points at indexes i, j, k, and l in list points
+    return shape(color, [points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1], points[l][0], points[l][1]], absolute=True, anchor='topleft')
 
 
 
 def create_World() -> World:
     vertices = []
     lines = []
+    faces = []
+    starting_scale = 100.0
     for index, point in enumerate(points):
         # @ is the matrix multiplication operator
         # Use transpose to change point from 1x3 to 3x1 matrix to make multiplication with 2d matrix compatible
         projected2d = projection_matrix @ point.transpose()
 
-        x = projected2d[0, 0] * scale + circle_pos[0]
-        y = projected2d[1, 0] * scale + circle_pos[1]
+        x = projected2d[0, 0] * starting_scale + circle_pos[0]
+        y = projected2d[1, 0] * starting_scale + circle_pos[1]
 
         vertices.append(circle("black", 5, x, y))
 
         projected_points[index] = [x, y]
 
-    lines.append(connect_points(0, 1, projected_points))
-    lines.append(connect_points(1, 2, projected_points))
-    lines.append(connect_points(2, 3, projected_points))
-    lines.append(connect_points(3, 0, projected_points))
 
-    lines.append(connect_points(4, 5, projected_points))
-    lines.append(connect_points(5, 6, projected_points))
-    lines.append(connect_points(6, 7, projected_points))
-    lines.append(connect_points(7, 4, projected_points))
+    for p in range(4):
+        lines.append(connect_points(p, (p+1) % 4, projected_points))
+        lines.append(connect_points(p+4, (p + 1) % 4 + 4, projected_points))
+        lines.append(connect_points(p, p+4, projected_points))
 
-    lines.append(connect_points(0, 4, projected_points))
-    lines.append(connect_points(1, 5, projected_points))
-    lines.append(connect_points(2, 6, projected_points))
-    lines.append(connect_points(3, 7, projected_points))
+    faces.append(create_face("red", 0, 1, 2, 3, projected_points))
+    faces.append(create_face("red", 4, 5, 6, 7, projected_points))
+    for p in range(4):
+        faces.append(create_face("red", p, (p + 1) % 4, (p + 1) % 4 + 4, p + 4, projected_points))
 
-    return World(0.0, vertices, lines)
+    return World(0.0, starting_scale, vertices, lines, faces)
+
 
 
 def main_loop(world: World):
 
 
     world.angle += 0.01
+
 
     rotation_x = np.matrix([
         [1, 0, 0],
@@ -111,32 +118,39 @@ def main_loop(world: World):
         # For each 3d coordinate, multiply by projection_matrix to convert to 2d coordinate
         projected2d = projection_matrix @ rotated2d
 
+        # Set projected x and y values for each coordinate
+        x = projected2d[0, 0] * world.scale + circle_pos[0]
+        y = projected2d[1, 0] * world.scale + circle_pos[1]
 
-        x = projected2d[0, 0] * scale + circle_pos[0]
-        y = projected2d[1, 0] * scale + circle_pos[1]
-
+        # Add x and y values to list of projected points
         projected_points[index] = [x, y]
 
-
+        # Move corresponding vertices to newly calculated positions
         world.vertices[index].x = x
         world.vertices[index].y = y
 
+    #Destroys current faces
+    for face in world.faces:
+        destroy(face)
+
+    # Generates 6 new faces
+    world.faces[0] = create_face("red", 0, 1, 2, 3, projected_points)
+    world.faces[1] = create_face("red", 4, 5, 6, 7, projected_points)
+    for p in range(4):
+        world.faces[p+2] = create_face("red", p, (p+1)%4, (p+1)%4+4, p+4, projected_points)
+
+    # Destroys current lines
     for line in world.lines:
         destroy(line)
-    world.lines[0] = connect_points(0, 1, projected_points)
-    world.lines[1] = connect_points(1, 2, projected_points)
-    world.lines[2] = connect_points(2, 3, projected_points)
-    world.lines[3] = connect_points(3, 0, projected_points)
 
-    world.lines[4] = connect_points(4, 5, projected_points)
-    world.lines[5] = connect_points(5, 6, projected_points)
-    world.lines[6] = connect_points(6, 7, projected_points)
-    world.lines[7] = connect_points(7, 4, projected_points)
+    # Generates 12 new lines
+    for p in range(4):
+        world.lines[p] = connect_points(p, (p+1) % 4, projected_points)
+        world.lines[p+4] = connect_points(p+4, (p + 1) % 4 + 4, projected_points)
+        world.lines[p+8] = connect_points(p, p+4, projected_points)
 
-    world.lines[8] = connect_points(0, 4, projected_points)
-    world.lines[9] = connect_points(1, 5, projected_points)
-    world.lines[10] = connect_points(2, 6, projected_points)
-    world.lines[11] = connect_points(3, 7, projected_points)
+
+
 
 
 
